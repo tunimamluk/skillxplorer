b2d08e28967865d4a9e8146ffcd98bd7dde276a9
tuni mamluk <tunimamluk@gmail.com>
2025-01-20 19:04:23 +0200

    Add initial homepage layout with category selection functionality

---
+++ homepage/script.js
@@ -0,0 +1,7 @@
+// פונקציה לבחירת קטגוריה
+function selectCategory(category) {
+    alert(`You selected the category: ${category}`);
+    // כאן תוכל להחליף עמוד או להציג את האתגרים של הנושא שנבחר
+    // לדוגמה, מעבר לדף חדש:
+    // window.location.href = `challenges.html?category=${category}`;
+  }
